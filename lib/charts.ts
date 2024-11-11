b7406dc213bdf7e01005544371753586f0295b92
Shehzar-Aurangzeb <shehzerabbasi621@gmail.com>
2024-11-11 15:33:07 -0500

    added created_at column in "avaialble consents to sell" modal and filtered consents with value equals to 0

--- lib/charts.ts
+++ lib/charts.ts
@@ -1,42 +1,43 @@
 import { TODAY } from '@/constants';
+import { DATATIMETYPE } from '@/constants/our_g_data';
 import moment from 'moment';
 
 export const getIntervalFromSelectedValue = (
-  selectedTimeRange: string,
+  selectedTimeRange: DATATIMETYPE,
 ): {
   numOfHour: number;
   interval: string[];
   relativeInterval: 'd' | 'm' | 'y' | 'h';
 } => {
-  if (selectedTimeRange === '1 YEAR') {
+  if (selectedTimeRange === DATATIMETYPE.YEAR) {
     return {
       numOfHour: 8766,
       interval: [moment().subtract(1, 'year').format('YYYY-MM-DD 00:00:00'), TODAY],
       relativeInterval: 'y',
     };
   }
-  if (selectedTimeRange === '1 DAY') {
+  if (selectedTimeRange === DATATIMETYPE.DAY) {
     return {
       interval: [moment().subtract(1, 'days').format('YYYY-MM-DD 00:00:00'), TODAY],
       numOfHour: 24,
       relativeInterval: 'd',
     };
   }
-  if (selectedTimeRange === '4 HOURS') {
+  if (selectedTimeRange === DATATIMETYPE.FOUR_HOURS) {
     return {
       interval: [moment().subtract(4, 'hours').format('YYYY-MM-DD HH:00:00'), moment().format('YYYY-MM-DD HH:00:00')],
       numOfHour: 4,
       relativeInterval: 'h',
     };
   }
-  if (selectedTimeRange === '15 HOURS') {
+  if (selectedTimeRange === DATATIMETYPE.FIFTEEN_HOURS) {
     return {
       interval: [moment().subtract(15, 'hours').format('YYYY-MM-DD HH:mm:00'), moment().format('YYYY-MM-DD HH:mm:00')],
       numOfHour: 15,
       relativeInterval: 'h',
     };
   }
-  if (selectedTimeRange === '1 HOURS') {
+  if (selectedTimeRange === DATATIMETYPE.HOUR) {
     return {
       interval: [moment().subtract(1, 'hours').format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')],
       numOfHour: 1,
